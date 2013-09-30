3273cede1fb5973123f25f8cd4a9c8d47bd559bf
SathishkumarKS <sathish.hari@gmail.com>
2013-09-30 16:42:50 +0530

    Code formatting

--- src/com/sathish/MyPhoneGapActivity.java
+++ src/com/sathish/MyPhoneGapActivity.java
@@ -4,9 +4,9 @@ import org.apache.cordova.DroidGap;
 import android.os.Bundle;
 
 public class MyPhoneGapActivity extends DroidGap {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		super.loadUrl("file:///android_asset/www/VoiceAlerter.html");
-	}
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    super.loadUrl("file:///android_asset/www/VoiceAlerter.html");
+  }
 }
